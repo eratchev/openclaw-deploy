2e7ae4a58ab505a3a217b16bb4ef8780d2a5a7fd
Evgueni Ratchev <evgueni.ratchev@docsend.com>
2026-03-17 09:39:45 -0700

    fix(contacts): add contacts.other.readonly scope for otherContacts.search endpoint

--- services/mail-proxy/scripts/auth_setup.py
+++ services/mail-proxy/scripts/auth_setup.py
@@ -12,6 +12,7 @@ SCOPES = [
     "https://www.googleapis.com/auth/gmail.send",
     "https://www.googleapis.com/auth/gmail.modify",
     "https://www.googleapis.com/auth/contacts.readonly",
+    "https://www.googleapis.com/auth/contacts.other.readonly",
 ]
 
 
